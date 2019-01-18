2a72180bf882e27834c9bf124a29990c0ed30148
Tyler Niccolls <tylerjniccolls@gmail.com>
2019-01-18 12:38:13 -0800

    functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,3 @@
-// var firstQuestions = ["Q1 P1 testing testing testing testing testing testing","Q1 P2", "Q1 P3", "Q1 P4"];
-// var secondQuestions = ["Q2 P1","Q2 P2", "Q2 P3", "Q2 P4"];
-// var thirdQuestions = ["Q3 P1","Q3 P2", "Q3 P3", "Q3 P4"];
-// var fourthQuestions = ["Q4 P1","Q4 P2", "Q4 P3", "Q4 P4"];
-// var fifthQuestions = ["Q5 P1","Q5 P2", "Q5 P3", "Q5 P4"];
-//Thor, Iron Man, Captain America, Hulk, Spiderman
-$("#backgroundPic").html("<img src= 'assets/images/ironman1.jpg' />");
-
 
 function buildQuiz() {
     const output = [];
@@ -58,6 +50,8 @@ function buildQuiz() {
     });
 
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    document.getElementById("timer").innerHTML = "Finished!";
+    timeLeft = 0;
   }
 
   const quizContainer = document.getElementById("quiz");
@@ -168,13 +162,21 @@ const myQuestions = [allQuestions[firstQuestionChoice], allQuestions[secondQuest
   submitButton.addEventListener("click", showResults);
 
 
-// var fourthQuestionChoice = Math.floor(Math.random()*3+9);
-// var fifthQuestionChoice = Math.floor(Math.random()*3+12);
 
+var timeLeft = 30;
+var clock = setInterval(function() {
 
+     timeLeft--;
 
-// $("#q1").text(firstQuestions[firstQuestionChoice]);
-// $("#q2").text(secondQuestions[secondQuestionChoice]);
-// $("#q3").text(thirdQuestions[thirdQuestionChoice]);
-// $("#q4").text(fourthQuestions[fourthQuestionChoice]);
-// $("#q5").text(fifthQuestions[fifthQuestionChoice]);
+    // Display the result in the element with id="demo"
+    document.getElementById("timer").innerHTML = "Time Remaining: " + timeLeft;
+  
+    // If the count down is finished, write some text 
+    if (timeLeft < 0) {
+      clearInterval(clock);
+      document.getElementById("timer").innerHTML = "Finished!";
+      showResults();
+    }
+
+  }, 1000);
+
